743ec25b600dae3699e438feea39ca60edf2c639
Rubow10 <rubow10@gmail.com>
2025-04-30 19:14:06 +0200

    ven

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="bg-cover bg-center min-h-screen flex items-center justify-center" 
          style={{ backgroundImage: `url("https://images.planetarium.dk/production/pages/stjerner.jpeg?w=1800&q=80&fm=webp&fit=max&dm=1728376127&s=ac90c49768d66809ad7eb9f538e56b2b")` }}>
       <h1 className="text-6xl font-bold text-white">
-        Venus In Leo
+        Venus In Leo ?
       </h1>
     </div>
   );
